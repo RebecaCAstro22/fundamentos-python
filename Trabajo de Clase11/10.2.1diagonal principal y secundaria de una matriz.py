47c9a25260752950a1aedb677d9da662816fbb07
Rebeca Michelle Castro Pineda <michell7castro7@gmail.com>
2025-06-14 11:59:03 -0600

    Tarea 11

---
+++ Trabajo de Clase11/10.2.1diagonal principal y secundaria de una matriz.py
@@ -0,0 +1,27 @@
+'''
+Clase:        1. 
+Tema:         Manejo de matrices
+Ejercicio:    10.2.1
+Descripción:  Extraer la diagonal principal y secundaria de una matriz 
+
+Autor:        Rebeca MIchelle Castro Pineda
+Fecha:        2025-06-14
+Estado:       [Terminado ]
+'''
+
+
+def obtener_diagonales(n, matriz):
+    diagonal_principal = [matriz[i][i] for i in range(n)]
+    diagonal_secundaria = [matriz[i][n - i - 1] for i in range(n)]
+    return diagonal_principal, diagonal_secundaria
+
+# Entrada del usuario con el mismo formato
+n = int(input("Ingrese el tamaño de la matriz: "))
+matriz = [list(map(int, input(f"Ingrese la fila {i + 1}: ").split(','))) for i in range(n)]
+
+# Obtener las diagonales
+diag_principal, diag_secundaria = obtener_diagonales(n, matriz)
+
+# Mostrar el resultado
+print(diag_principal)
+print(diag_secundaria)
